d1a45bae8ed33176595142458aef03523b67ceea
WielderSilver <silverbeamskittles@gmail.com>
2022-11-07 15:08:58 -0500

    Initial commit

---
+++ src/main/java/net/silverbeam/scmkeyblades/ScmKeybladesClient.java
@@ -0,0 +1,10 @@
+package net.silverbeam.scmkeyblades;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ScmKeybladesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
